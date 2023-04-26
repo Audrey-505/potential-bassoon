a3430091de2614818335ab4eddf23b25c6ea934e
Audrey Alcantara <adrianaquin22@gmail.com>
2023-04-26 10:16:29 -0400

    search bar

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Search(){
-    let test = async () => {
-        try{
-            let res = await fetch(`https://pokeapi.co/api/v2/pokemon/clefairy/`)
-            let results = await res.json()
-            console.log(results)
-            // return results
-        }catch(error){
-            console.log(error)
-        }
-      }
-      test()
-
+    const [pName, setPName] = useState('')
+    console.log(pName)
+    // let test = async () => {
+    //     try{
+    //         let res = await fetch(`https://pokeapi.co/api/v2/pokemon/clefairy/`)
+    //         let results = await res.json()
+    //         console.log(results)
+    //         // console.log(results.name)
+    //         // return results
+    //     }catch(error){
+    //         console.log(error)
+    //     }
+    //   }
+    //   test()
     return (
         <>
         <h1>Search Page</h1>
+        <input type='text' value={pName} placeholder='Pokemon Name' onChange={(event) => {
+            setPName(event.target.value)
+        }}/>
+        <button type='submit'>Go!</button>
         </>
     )
 }
