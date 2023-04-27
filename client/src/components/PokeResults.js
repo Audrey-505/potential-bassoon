82de6787d51bf82499a954fb8b2d6bb07c55944a
Audrey Alcantara <adrianaquin22@gmail.com>
2023-04-27 15:43:28 -0400

    styling add ons and modifications to results page

--- client/src/components/PokeResults.js
+++ client/src/components/PokeResults.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
-import search from './search'
-import { Container, Col } from 'react-bootstrap'
+import { Container, Col, Row } from 'react-bootstrap'
 
 export default function PokeResults({pName}){
     console.log(pName)
+    const picStyle = {width: '300px', height: '300px'}
+    const title = {justifyContent: 'center', display: 'flex'}
     const [pokemon, setPokemon] = useState('')
     const [pokemonId, setPokemonId] = useState('')
     const [pokemonPic, setPokemonPic] = useState('')
+    const [xp, setXp] = useState('')
      let test = async () => {
         try{
             let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pName}/`)
@@ -16,6 +18,7 @@ export default function PokeResults({pName}){
             setPokemon(results.name)
             setPokemonId(results.id)
             setPokemonPic(results.sprites.other.home.front_default)
+            setXp(results.base_experience)
             // console.log(results.sprites.other.home.front_default)
         }catch(error){
             console.log(error)
@@ -26,13 +29,21 @@ export default function PokeResults({pName}){
     return(
         <>
         <Container>
-        <h1>Pokemon Data</h1>
+        <h1 style={title}>Pokemon Data</h1>
         </Container>
 
         <Container>
+            <Container>
             <Col><h1>Pokemon Name: {pokemon}</h1></Col>
             <Col><h1>Pokemon ID: {pokemonId}</h1></Col>
-            <Col><h1>Pokemon Picture:</h1><img src={pokemonPic}/></Col>
+            <Col><h1>XP: {xp}</h1></Col>
+            </Container>
+            <Container>
+            <Row>
+                <Col><h1>Pokemon Picture:</h1></Col>
+                <Col><img src={pokemonPic} style={picStyle} alt='pokemon'/></Col>
+            </Row>
+            </Container>
         </Container>
         </>
     )
