5ac9a99fb6d0465444b40b888456c21977e167a0
Audrey Alcantara <adrianaquin22@gmail.com>
2023-04-27 10:09:06 -0400

    working on set up of results, got the name out of data

--- client/src/components/PokeResults.js
+++ client/src/components/PokeResults.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import search from './search'
+import { Container, Col } from 'react-bootstrap'
 
 export default function PokeResults({pName}){
     console.log(pName)
+    const [pokemon, setPokemon] = useState('')
      let test = async () => {
         try{
             let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pName}/`)
             let results = await res.json()
             console.log(results)
             // console.log(results.name)
-            // return results
+            setPokemon(results.name)
         }catch(error){
             console.log(error)
         }
@@ -18,7 +20,13 @@ export default function PokeResults({pName}){
 
     return(
         <>
+        <Container>
         <h1>Pokemon Data</h1>
+        </Container>
+
+        <Container>
+            <Col><h1>Pokemon Name: {pokemon}</h1></Col>
+        </Container>
         </>
     )
 }
